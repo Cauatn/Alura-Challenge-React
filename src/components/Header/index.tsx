88762edbdda8e4cf38ae67f886f3462a15270df7
Cauã Tavares <39890456+CauanGx@users.noreply.github.com>
2022-03-25 20:52:38 -0300

    Header com media Query finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import Logo from '../Logo';
 import NavBar from '../NavBar';
 import Botao from '../Botao';
 import style from './Header.module.scss';
+import BotaoPesquisa from '../BotaoPesquisa';
 
 export default function Header() {
     return (
@@ -12,6 +13,7 @@ export default function Header() {
                 <NavBar />
             </div>
             <Botao texto={'Login'}/>
+            <BotaoPesquisa />
         </div>
     )
 }
