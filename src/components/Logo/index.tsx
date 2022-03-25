8dcc693349ffad57ed072094970ea7b6ecff2fe2
Cauã Tavares <39890456+CauanGx@users.noreply.github.com>
2022-03-25 20:07:39 -0300

    ajustando media query da logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,17 +1,32 @@
-import Controle from '../../assets/img/LogoN/ControleN.png';
+import ControleN from '../../assets/img/LogoN/ControleN.png';
 
-import Alura from '../../assets/img/LogoN/AluraN.png';
+import AluraN from '../../assets/img/LogoN/AluraN.png';
 
-import Geek from '../../assets/img/LogoN/GeekN.png';
+import GeekN from '../../assets/img/LogoN/GeekN.png';
+
+import ControleP from '../../assets/img/LogoP/ControleP.png';
+
+import AluraP from '../../assets/img/LogoP/AluraP.png';
+
+import GeekP from '../../assets/img/LogoP/GeekP.png';
 
 import style from './Logo.module.scss';
+
+
 
 export default function Logo() {
     return(
         <div className={style.logo}>
-           <img src={Controle} alt="imagem logo controle" />
-           <img src={Alura} alt="imagem logo alura" />
-           <img src={Geek} alt="imagem logo geek" />
+            <div className={style.logoN}>
+                <img src={ControleN} alt="imagem logo controle" />
+                <img src={AluraN} alt="imagem logo alura" />
+                <img src={GeekN} alt="imagem logo geek" />
+            </div>
+           <div className={style.logoP}>
+                <img src={ControleP} alt="imagem logo controle" />
+                <img src={AluraP} alt="imagem logo alura" />
+                <img src={GeekP} alt="imagem logo geek" />
+           </div>
         </div>
     )
 }
