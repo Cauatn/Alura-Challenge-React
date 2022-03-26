6727d6ef7b7d9eb6d77268bd19e974c7705d18d2
Cauã Tavares <39890456+CauanGx@users.noreply.github.com>
2022-03-26 10:21:42 -0300

    Estilização do footer concluida (sem media Query)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,21 @@
+import Formulario from '../Formulario';
+import ListaLink from '../ListaLink';
+import Logo from '../Logo';
 import style from './Footer.module.scss';
 
 export default function Footer() {
     return(
         <footer className={style.footer}>
-            <div className={style.primeiroContainer}>
-                <div className={style.containerConteudo}></div>
-            </div>
-            <div className={style.segundoContainer}>
-                <p>Desenvolvido por Cauã Tavares<br></br>2022</p>
+            <div className={style.footerContainer}>
+                <div className={style.primeiroContainer}>
+                    <Logo/>
+                    <ListaLink />
+                    <Formulario />
+                </div>
+                <div className={style.segundoContainer}>
+                        <p >Desenvolvido por Cauã Tavares</p>
+                        <p>2022</p>
+                </div>
             </div>
         </footer>
     )
---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Botao from "../Botao";
+import style from './Formulario.module.scss';
+
+export default function Formulario() {
+    return(
+        <form
+            className={style.Formulario}
+            action="">
+                <p 
+                    className={style.formularioTitulo}
+                >
+                    Fale Conosco
+                </p>
+                <div className={style.inputContainer}>
+                    <label htmlFor="nome" className={style.inputNameLabel}>Nome</label>
+                    <input
+                        id="name"
+                        className={style.inputName}
+                        type="text"
+                    />
+                </div>
+                <textarea
+                    className={style.inputMensagem}
+                    placeholder='Envie sua mensagem'
+                />
+                <Botao
+                    texto={'Enviar mensagem'}
+                />
+        </form>
+    )
+}
