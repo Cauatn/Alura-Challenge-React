85062d2cd1719cfe0d1686ad00121732b4e78f04
Cauã Tavares <39890456+Cauatn@users.noreply.github.com>
2023-06-27 08:52:07 -0300

    Update index.tsx
    atualizando LoginButton

--- src/components/BotaoPesquisa/index.tsx
+++ src/components/BotaoPesquisa/index.tsx
@@ -1,7 +1,12 @@
-import style from './BotaoPesquisa.module.scss';
+import { Link } from 'react-router-dom';
+import style from './Botao.module.scss';
 
-export default function BotaoPesquisa() {
+interface Props {
+    texto : string
+}
+
+export default function LoginButton({texto} : Props) {
     return(
-        <button className={style.BotaoPesquisa}></button>
+        <Link to="/Login" ><button className={style.Botao}>{texto}</button></Link>
     )
 }
